63b305f47407369763bd8dc24138bac6d98f82b9
adamczykm <m@adamczykm.me>
2022-06-24 18:12:20 +0200

    Fix create pr body script

--- add_support_channels/create_pr_body.sh
+++ add_support_channels/create_pr_body.sh
@@ -5,7 +5,7 @@ ISSUE_NUM=$2
 
 echo "#### Ecosystem defolkloring effort
 
-This automatic draft pull request appends a header with a placeholder for "support channel" information to the repository **README.md**
+This automatic draft pull request appends a header with a placeholder for \"support channel\" information to the repository **README.md**
 
 Please take over the PR & replace the placeholder with relevant information.
 
